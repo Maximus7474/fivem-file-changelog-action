9a670c9982273b0155c675eb08e2b000b06ab4c3
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-27 12:25:30 +0100

    feat(action): ignore_patterns input

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export interface ActionConfig {
   discordWebhook: string | false;
   changelogDir: string | false;
   changelogFilename: string | false;
+  ignorePatterns: string[];
 }
 
 export function getConfig(): ActionConfig {
@@ -12,11 +13,18 @@ export function getConfig(): ActionConfig {
   const discordWebhook = core.getInput('webhook', { required: false }) || false;
   const changelogDir = core.getInput('changelog', { required: false }) || false;
   const changelogFilename = core.getInput('changelog_filename', { required: false }) || false;
+  const ignorePatternsInput = core.getInput('ignore_patterns', { required: true });
+
+  const ignorePatterns = ignorePatternsInput
+    .split('\n')
+    .map(p => p.trim())
+    .filter(p => p.length > 0);
 
   return {
     githubToken,
     discordWebhook,
     changelogDir,
     changelogFilename,
+    ignorePatterns,
   };
 }
