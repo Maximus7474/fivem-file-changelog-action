ed7977b7318733fa62eab4c9e27d9fb091482701
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-27 12:11:05 +0100

    feat: initial index wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import * as core from '@actions/core';
+import { SendWebhook } from './webhook';
+import { SaveChangelog } from './saveChangelog';
+import { getFileChangelog } from './getChangelog';
+import { CHANGELOG_DIR, DISCORD_WEBHOOK } from './config';
+
+export async function run() {
+  try {
+    const data = await getFileChangelog();
+
+    if (!data) return;
+    const { changedFiles, version } = data;
+
+    if (DISCORD_WEBHOOK) await SendWebhook(version, changedFiles, core);
+    if (CHANGELOG_DIR) await SaveChangelog(version, changedFiles, core);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed('An unexpected error occurred.');
+    }
+  }
+}
+
+if (!process.env.JEST_WORKER_ID) {
+  run();
+}
